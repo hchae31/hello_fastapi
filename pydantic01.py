1c93de8926a9666647ad5e1888107d4134ac675a
hchae31 <chaeheegon5858@gmail.com>
2024-08-13 11:37:24 +0900

    pydantic - sungjuk 모델 자동생성 및 상세 조회

--- pydantic01.py
+++ pydantic01.py
@@ -14,7 +14,7 @@ class Sungjuk(BaseModel):
     name: str
     kor: int
     eng: int
-    math: int
+    mat: int
 
 # 성적 데이터 저장용 변수
 sungjuk_db: List[Sungjuk] = []
@@ -29,9 +29,42 @@ def index():
 # 성적데이터 조회
 # reponse_model: 응답데이터 형식 지정
 # 이를 통해서 클라이언트는 서버가 어떤 종류의 데이터를 응답으로 보내는지 알수 있음
+
 @app.get('/sj', response_model=List[Sungjuk])
 def sj_readall():
     return sungjuk_db
+
+# 성적 데이터 추가
+# FastAPI swagger UI 이용 : http://127.0.0.1:8000/docs
+# 클라이언트가 요청시 성적데이터는 json으로 구성되어야함!
+
+@app.post('/sjadd', response_model=Sungjuk)
+def sj_create(sj: Sungjuk):
+    # sj.name = ('혜교',99,98,99).json()
+    sungjuk_db.append(sj)
+    return sj
+
+#샘플 성적데이터 추가 : 3건의 기본 데이터
+@app.get('/sjadd', response_model=Sungjuk)
+def sj_create():
+    sj = Sungjuk(name='혜교', kor=99, eng=98, mat=99)
+    sungjuk_db.append(sj)
+
+    sj = Sungjuk(name='지현', kor=59, eng=68, mat=89)
+    sungjuk_db.append(sj)
+
+    sj = Sungjuk(name='수지', kor=19, eng=93, mat=59)
+    sungjuk_db.append(sj)
+    return sj
+
+# 성적데이터 상세 조회 - 이름으로 조회
+@app.get('/sjone/{name}', response_model=Sungjuk)
+def sjone(name: str):
+    findone = Sungjuk(name='none', kor=00, eng=00, mat=00)
+    for sj in sungjuk_db:
+        if sj.name == name:
+            findone = sj
+    return findone
 
 if __name__ == "__main__":
     import uvicorn
